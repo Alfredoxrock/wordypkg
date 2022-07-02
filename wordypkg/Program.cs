171f404213c4bcfa7d179baebcceb6d65b58f435
Alfredoxrock <alf_baza@hotmail.com>
2022-07-01 19:05:13 -0600

    Added new methods

--- wordypkg/Program.cs
+++ wordypkg/Program.cs
@@ -21,26 +21,146 @@ namespace wordypkg
                 Console.WriteLine("Please enter a vocabulary word");
                 Environment.Exit(0);
             }
-
-            string query = args[0];
-            
-            //This is a request from a website and it will return a string
-            using (var webClient = new System.Net.WebClient())
+            request(args[0]);
+            if(args[1] != null && args[1] == "example")
             {
-                var json = webClient.DownloadString("https://dictionaryapi.com/api/v3/references/thesaurus/json/"+ query +"?key=" + key.keyk);
-                // Now parse with JSON.Net
-                var obj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
-                var readable = Newtonsoft.Json.JsonConvert.SerializeObject(obj, Newtonsoft.Json.Formatting.Indented);
+                examples(args[0]);
+            }
+            else if(args[1] != null && args[1] == "synonyms")
+            {
+                synonym(args[0]);
+            }
+            else if(args[1] != null && args[1] == "antonyms")
+            {
+                antonym(args[0]);
+            }
 
-                JArray jsonArray = JArray.Parse(readable);
-                //string defi = (string)text[0]["def"];
-                //Console.WriteLine("Definition: " + jsonArray[0]["def"][0]["sseq"][0][0][1]["dt"][0][1]);
-                Console.WriteLine("Definition: " + jsonArray[0]["shortdef"].ToString().Trim('[').Trim(']').Replace("\"",""));
 
 
+        }
+
+        public static void examples(string query)
+        {
+            try
+            {
+                //This is a request from a website and it will return a string
+                using (var webClient = new System.Net.WebClient())
+                {
+                    var json = webClient.DownloadString("https://dictionaryapi.com/a" +
+                        "pi/v3/references/thesaurus/json/" + query + "?key=" + key.keyk);
+                    // Now parse with JSON.Net
+                    var obj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                    var readable = Newtonsoft.Json.JsonConvert.SerializeObject(obj,
+                        Newtonsoft.Json.Formatting.Indented);
+
+                    JArray jsonArray = JArray.Parse(readable);
+                    //string defi = (string)text[0]["def"];
+                    //Console.WriteLine("Definition: " +
+                    //jsonArray[0]["def"][0]["sseq"][0][0][1]["dt"][0][1]);
+                    Console.WriteLine("Example: " +
+                        jsonArray[0]["def"][0]["sseq"][0][0][1]["dt"][1][1][0]["t"]);
+
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Examples not found");
+            }
+        }
+
+        public static void antonym(string query)
+        {
+            try
+            {
+                //This is a request from a website and it will return a string
+                using (var webClient = new System.Net.WebClient())
+                {
+                    var json = webClient.DownloadString("https://dictionaryapi.com/a" +
+                        "pi/v3/references/thesaurus/json/" + query + "?key=" + key.keyk);
+                    // Now parse with JSON.Net
+                    var obj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                    var readable = Newtonsoft.Json.JsonConvert.SerializeObject(obj,
+                        Newtonsoft.Json.Formatting.Indented);
+
+                    JArray jsonArray = JArray.Parse(readable);
+                    //string defi = (string)text[0]["def"];
+                    //Console.WriteLine("Definition: " +
+                    //jsonArray[0]["def"][0]["sseq"][0][0][1]["dt"][0][1]);
+                    Console.WriteLine("Antonymns: " +
+                        jsonArray[0]["meta"]["ants"][0].ToString().Trim('[')
+                            .Trim(']').Replace("\"", ""));
+
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Antonyms not found");
+            }
+        }
+
+        public static void synonym(string query)
+        {
+            try
+            {
+                //This is a request from a website and it will return a string
+                using (var webClient = new System.Net.WebClient())
+                {
+                    var json = webClient.DownloadString("https://dictionaryapi.com/a" +
+                        "pi/v3/references/thesaurus/json/" + query + "?key=" + key.keyk);
+                    // Now parse with JSON.Net
+                    var obj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                    var readable = Newtonsoft.Json.JsonConvert.SerializeObject(obj,
+                        Newtonsoft.Json.Formatting.Indented);
+
+                    JArray jsonArray = JArray.Parse(readable);
+                    //string defi = (string)text[0]["def"];
+                    //Console.WriteLine("Definition: " +
+                    //jsonArray[0]["def"][0]["sseq"][0][0][1]["dt"][0][1]);
+                    Console.WriteLine("Synonyms: " +
+                        jsonArray[0]["meta"]["syns"][0].ToString().Trim('[')
+                            .Trim(']').Replace("\"", ""));
+
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Synonyms not found");
+            }
+        }
+
+        public static void request(string query)
+        {
+            try
+            {
+                //This is a request from a website and it will return a string
+                using (var webClient = new System.Net.WebClient())
+                {
+                    var json = webClient.DownloadString("https://dictionaryapi.com/a" +
+                        "pi/v3/references/thesaurus/json/" + query + "?key=" + key.keyk);
+                    // Now parse with JSON.Net
+                    var obj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                    var readable = Newtonsoft.Json.JsonConvert.SerializeObject(obj,
+                        Newtonsoft.Json.Formatting.Indented);
+
+                    JArray jsonArray = JArray.Parse(readable);
+                    //string defi = (string)text[0]["def"];
+                    //Console.WriteLine("Definition: " +
+                    //jsonArray[0]["def"][0]["sseq"][0][0][1]["dt"][0][1]);
+                    Console.WriteLine("Definition: " +
+                        jsonArray[0]["shortdef"].ToString().Trim('[')
+                            .Trim(']').Replace("\"", ""));
+
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Word not found");
             }
             
-
         }
 
         
